8b6805fea51b0ebacf679eab6f4061c4b1ce6111
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-20 00:55:55 -1000

    moved cache into the data module

--- config/constants.go
+++ config/constants.go
@@ -5,6 +5,6 @@ const (
 	DateFormat       = "2006-01-02"
 	TimeFormat       = "2006-01-02 15:04:05 -0700"
 	ICloudDirPath    = "/Users/saavedj/Library/Mobile Documents/com~apple~CloudDocs/health-data"
-	CacheFilePath    = "cache.json"
+	CacheFilePath    = "data/cache.json"
 	DateRegexPattern = `\d{4}-\d{2}-\d{2}`
 )
