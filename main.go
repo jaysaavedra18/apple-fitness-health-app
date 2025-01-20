812a914b90e1910f9dc6f80af030cda3e0ea09e1
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-19 14:10:24 -1000

    data read in, attempting to parse out keys

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
 )
+
+type Health struct {
+	Workouts struct {
+		name string `json:"name"`
+
+	} `json:"workouts"`
+}
 
 func main() {
 	// Open the directory (iCloud Drive)
@@ -17,17 +25,27 @@ func main() {
 	// Sift through the files for json files
 	for _, file := range files {
 		if strings.HasSuffix(file.Name(), ".json") {
-			filePath := dirPath + "/" + file.Name()
+			// filePath := dirPath + "/" + file.Name()
+			filePath := "/Users/saavedj/Library/Mobile Documents/com~apple~CloudDocs/health-data/HealthAutoExport-2024-10-21-2025-01-19.json"
 
 			content, err := os.ReadFile(filePath)
 			if err != nil {
 				panic(err)
 			}
 
-			fmt.Printf("File: %s\n", file.Name())
-			fmt.Println(string(content))
+			var jsonData map[string]interface{}
+			err = json.Unmarshal(content, &jsonData)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println("Fields (keys):")
+			for key := range jsonData {
+				fmt.Println(key)
+			}
+
+
 		}
 	}
-
 
 }
