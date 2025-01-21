e3a970d81c38edcc8f616e99f273aabec1070320
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-21 10:54:41 -1000

    added testing package and data_test suite for data package

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 
 import (
 	"fitness/data"
-	"fmt"
 )
 
 func main() {
@@ -14,18 +13,5 @@ func main() {
 	// cli.StartCLI()
 
 	// Run the RESTful API Server
-	workouts1, ok1 := data.FilterWorkoutData("Outdoor Run")
-	fmt.Printf("Test 1 - Data:\n %v, Success: %t\n", workouts1, ok1)
 
-	// Test 2: Filter for "Outdoor Run, Indoor Run"
-	workouts2, ok2 := data.FilterWorkoutData("Outdoor Run, Indoor Run")
-	fmt.Printf("Test 2 - Data:\n %v, Success: %t\n", workouts2, ok2)
-
-	// Test 3: Filter for "Outdoor Run, Indoor Run, Pool Swim"
-	workouts3, ok3 := data.FilterWorkoutData("Outdoor Run, Indoor Run, Pool Swim")
-	fmt.Printf("Test 3 - Data:\n %v, Success: %t\n", workouts3, ok3)
-
-	// Test 4: Filter for "Sky Dive" (no match)
-	workouts4, ok4 := data.FilterWorkoutData("Sky Dive, outdoor Run")
-	fmt.Printf("Test 4 - Data:\n %v, Success: %t\n", workouts4, ok4)
 }
