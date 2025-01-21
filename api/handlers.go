dcd1984e01d3ca7e9e2995289b238fdaaee81cb4
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-21 11:01:56 -1000

    handlers moved to proper file handlers.go

--- api/handlers.go
+++ api/handlers.go
@@ -1,4 +1,41 @@
 // api/handlers.go
 package api
 
+import (
+	"encoding/json"
+	"fitness/data"
+	"net/http"
+)
+
 // Each handler corresponds to a different endpoint in the API
+// Define API endpoints and map them to their respective handlers
+func HandleWorkoutData(w http.ResponseWriter, r *http.Request) {
+	// Handle workout data
+	switch r.Method {
+	case http.MethodGet:
+		GetWorkoutData(w, r)
+	case http.MethodPatch:
+		UpdateWorkoutData(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func GetWorkoutData(w http.ResponseWriter, r *http.Request) {
+	// Handle workout query and errors fetching data
+	var workoutType = r.URL.Query().Get("workoutType")
+	workoutData, ok := data.FilterWorkoutData(workoutType)
+	if !ok || workoutData == nil {
+		http.Error(w, "Error filtering workout data", http.StatusInternalServerError)
+		return
+	}
+
+	// Set response header and return the filtered workout data as JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(workoutData)
+
+}
+
+func UpdateWorkoutData(w http.ResponseWriter, r *http.Request) {
+	// Update workout data
+}
