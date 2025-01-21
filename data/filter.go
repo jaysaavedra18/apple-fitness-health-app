95f88a0f64e7ceed6b4a7115d77667b58bb6141a
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-21 12:51:34 -1000

    added filter function definition and handler for another filter

--- data/filter.go
+++ data/filter.go
@@ -1,4 +1,5 @@
 // data/filter.go
+// Filters for workout data
 
 package data
 
@@ -7,10 +8,10 @@ import (
 	"strings"
 )
 
-func FilterWorkoutData(workoutType string) ([]models.Workout, bool) {
+func FilterWorkout(workouts []models.Workout, workoutType string) ([]models.Workout, bool) {
 	// If workout type is empty, return all workouts
 	if workoutType == "" {
-		return AllWorkouts, true
+		return workouts, true
 	}
 
 	// Split the workout type into workout names if multiple present
@@ -21,7 +22,7 @@ func FilterWorkoutData(workoutType string) ([]models.Workout, bool) {
 
 	// Filter the workout data based on the workout type
 	var filteredWorkouts []models.Workout
-	for _, workout := range AllWorkouts { // Check each workout for match
+	for _, workout := range workouts { // Check each workout for match
 		for _, name := range targetNames { // Check each target name for match
 			if strings.EqualFold(workout.Name, name) {
 				filteredWorkouts = append(filteredWorkouts, workout)
@@ -32,4 +33,8 @@ func FilterWorkoutData(workoutType string) ([]models.Workout, bool) {
 
 	// Return the filtered workouts and a boolean indicating if any were found
 	return filteredWorkouts, len(filteredWorkouts) > 0
+}
+
+func FilterCalories(workouts []models.Workout, calorieThreshold float64) ([]models.Workout, bool) {
+	return nil, false
 }
