6a2d555bf5bebcdd79cdfb10f37f590cf247b60f
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-21 16:09:51 -1000

    filter start and end date works on endpoint

--- main.go
+++ main.go
@@ -2,8 +2,10 @@
 package main
 
 import (
+	"fitness/api"
 	"fitness/data"
 	"fmt"
+	"net/http"
 )
 
 func main() {
@@ -14,16 +16,10 @@ func main() {
 	// cli.StartCLI()
 
 	// Run the RESTful API Server
-	// api.RegisterRoutes()
-	// fmt.Println("Starting server on :8080")
-	// if err := http.ListenAndServe(":8080", nil); err != nil {
-	// 	fmt.Println("Error starting server:", err)
-	// }
-
-	w1, ok1 := data.FilterDate(data.AllWorkouts, "2024-12-01", true)
-	w2, ok2 := data.FilterDate(data.AllWorkouts, "2023-12-25", false)
-
-	fmt.Println("Filtered workouts by start date:", w1[0], ok1)
-	fmt.Println("Filtered workouts by end date:", w2[0], ok2)
+	api.RegisterRoutes()
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 
 }
