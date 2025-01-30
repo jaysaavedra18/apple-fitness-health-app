55040e37cc105bbad99ee73e32ba88bedff5c15f
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-30 11:33:44 -1000

    new updates to landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -28,11 +28,11 @@ export default function Landing() {
           </div>
           <div className="text-center">
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-              Data to enrich your online business
+              Visualize your health today
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
+              {/* Track trends, analyze progress, and uncover insights from your
+              Apple Health & Fitness data—all in one place. */}
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
