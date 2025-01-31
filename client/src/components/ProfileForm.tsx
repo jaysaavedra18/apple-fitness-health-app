9f24f3e44fe4fd915f00131e790cb51951fa40ff
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-30 16:10:23 -1000

    importing profile form

--- client/src/components/ProfileForm.tsx
+++ client/src/components/ProfileForm.tsx
@@ -1,2 +1,31 @@
 import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import { ChevronDownIcon } from "@heroicons/react/16/solid";
+
+export default function Example() {
+  return (
+    <form>
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base/7 font-semibold text-gray-900">Profile</h2>
+          <p className="mt-1 text-sm/6 text-gray-600">
+            This information will be displayed publicly so be careful what you share.
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-4">
+              <label htmlFor="username" className="block text-sm/6 font-medium text-gray-900">
+                Username
+              </label>
+              <div className="mt-2">
+                <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                  <div className="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">workcation.com/</div>
+                  <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    placeholder="janesmith"
+                    className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                  />
+                </div>
+              </div>
+            </div>
