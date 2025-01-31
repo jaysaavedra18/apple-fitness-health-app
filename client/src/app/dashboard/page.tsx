090076ff114a6373ce12eafdf09c7b170b800d55
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-30 16:22:44 -1000

    remvoed the background from the collections pane

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -29,7 +29,7 @@ const callouts = [
 
 export default function Dashboard() {
   return (
-    <div className="bg-gray-100">
+    <div>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
           <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
