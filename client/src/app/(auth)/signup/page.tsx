317b3d36ee83c849bc9f4229990c60ba8170bb25
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-02-04 14:52:43 -1000

    updating the register page with a confirm password section

--- client/src/app/(auth)/signup/page.tsx
+++ client/src/app/(auth)/signup/page.tsx
@@ -47,16 +47,32 @@ export default function Signup() {
                 <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
                   Password
                 </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
               </div>
               <div className="mt-2">
                 <input
                   id="password"
                   name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 
+                  outline -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 
+                  focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  placeholder="strongpassword123"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                  Confirm Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="confirm-password"
+                  name="confirm-password"
                   type="password"
                   required
                   autoComplete="current-password"
